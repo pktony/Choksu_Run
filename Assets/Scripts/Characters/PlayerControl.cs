8ea6f715ddf7887219e89c273b2ccb57dad27837
pktony@naver.com <66198932+pktony@users.noreply.github.com>
2022-12-16 15:04:45 +0900

    [프로젝트 기본 세팅] 기본 폴더 세팅
    폴더 세팅
     - Prefabs, Scripts, Materials ....

---
+++ Assets/Scripts/Characters/PlayerControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private void Awake()
+    { 
+    }
+}
